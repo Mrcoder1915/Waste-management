93f3edbde6ad0103b95a0b999834b7c49ca97459
Mrcoder1915 <sammynavarro001@gmail.com>
2026-07-21 03:06:48 +0800

    build(api): migrate hono app to cloudflare workers

---
+++ packages/api/src/types/env.ts
@@ -0,0 +1,9 @@
+export interface env {
+  DATABASE_URL: string;
+  AUTH_SECRET: string;
+  AUTH_BASE_URL: string;
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+  RESEND_API_KEY: string;
+  APP: "production" | "development" | "local";
+}
